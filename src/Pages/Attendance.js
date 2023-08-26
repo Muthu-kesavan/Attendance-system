609fe37f91d945851a808f2919d25276898bda65
Muthuk07 <muthukesavan6044@gmail.com>
2023-08-26 15:35:16 +0530

    Day 1 - Commit

---
+++ src/Pages/Attendance.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import AttendanceList from '../Components/AttendanceList'
+import { Attendivisor } from '../adjacer/Attendivisor'
+import "../Styles/Attendance.css"
+function Attendance() {
+  return (
+    <div className="attendance">
+      <h1>Attendance</h1>
+      <div className="attendivisor">
+        { Attendivisor.map((attendance, idx) => {
+          return(
+            <AttendanceList id={idx} name={attendance.name} image={attendance.image}/>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Attendance

e0030145f691042c69540b62b1a1ed5af9e2146f
Muthuk07 <muthukesavan6044@gmail.com>
2023-08-26 10:48:35 +0530

    Day 1 - Commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
-import{BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Attendance from './Pages/Attendance';
+import Navbar from './Components/Navbar';
+import Attendance from'./Pages/Attendance';
+import Footer from'./Components/Footer';
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
-          
+          <Route path="/" element={<Home/>} />
+          <Route path="/Attendance" element={<Attendance/>} />
         </Routes>
+        <Footer />
       </Router>
-      
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -12,18 +12,18 @@ function Navbar() {
     }, [location]);
   return (
     <div className="navbar" id={expandNavbar ? "open" : "close"}>
-    <div className="toggle">
-    <button onClick={() =>{
-        setExpandNavbar((prev) => !prev);
-    }}
-    >
-        <ReorderIcon />
-    </button>
-    </div>
-    <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/Attendance">Attendance</Link>
-    </div>
+        <div className="toggle">
+            <button onClick={() =>{
+                setExpandNavbar((prev) => !prev);
+            }}
+            >
+                <ReorderIcon />
+            </button>
+        </div>
+        <div className="links">
+            <Link to="/">Home</Link>
+            <Link to="/Attendance">Attendance</Link>
+        </div>
     </div>
 
   )

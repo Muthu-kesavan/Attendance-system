4c51e4012fe88fd4f89bd599dd3b9257e2f8a65f
Muthuk07 <muthukesavan6044@gmail.com>
2023-08-26 10:02:37 +0530

    Day 1 - Commit

---
+++ src/adjacer/Attendivisor.js
@@ -0,0 +1,12 @@
+import student from "../assets/student.jpg";
+import teacher from "../assets/image2.jpg";
+export const Attendivisor = [
+    {
+        name: "For Students",
+        image: student
+    },
+    {
+        name: "For Faculties",
+        image: teacher
+    },
+];
